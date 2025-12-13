b9c1eb7bda39be1657f17f7670aceaed9d5169dc
Md. Nazim Hasan <connect.nazimhasan@gmail.com>
2025-12-13 18:34:24 +0600

    chore (refactor): breakdown auth route and ui improvement

--- App.tsx
+++ App.tsx
@@ -2,11 +2,11 @@ import 'react-native-url-polyfill/auto';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './src/store';
-import AppNavigator from './src/navigation/AppNavigator';
+import RootNavigator from './src/navigation/RootNavigator';
 export default function App() {
   return (
     <Provider store={store}>
-      <AppNavigator />
+      <RootNavigator />
     </Provider>
   );
 }
---
+++ src/navigation/AuthNavigator.tsx
@@ -0,0 +1,20 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../screens/LoginScreen";
+import SignupScreen from "../screens/SignupScreen";
+
+const AuthNavigator = () => {
+  const Stack = createNativeStackNavigator();
+  return (
+    <Stack.Navigator
+      initialRouteName="Login"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default AuthNavigator;
