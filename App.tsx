b88b5e23670df5f8e8a8c01346e2fdc66ff924e6
Md. Nazim Hasan <connect.nazimhasan@gmail.com>
2025-12-13 20:26:20 +0600

    feat (persist): added persistor mmkv

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
-import 'react-native-url-polyfill/auto';
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
+import 'react-native-url-polyfill/auto';
 import { Provider } from 'react-redux';
-import { store } from './src/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import RootNavigator from './src/navigation/RootNavigator';
+import { persistor, store } from './src/store';
 export default function App() {
   return (
     <Provider store={store}>
-      <RootNavigator />
+      <PersistGate loading={<ActivityIndicator />} persistor={persistor}>
+        <RootNavigator />
+      </PersistGate>
     </Provider>
   );
 }
